36dc2ed7b76cab891ace4b00e380e6580f9f1d79
zdenekd <ditezdenek@gmail.com>
2020-03-20 18:43:13 +0100

    Run eslint .]

--- src/index.js
+++ src/index.js
@@ -1,38 +1,39 @@
 // Modules
-const {app, BrowserWindow} = require('electron')
+const {app, BrowserWindow} = require('electron');
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
-let mainWindow
+let mainWindow;
 
 // Create a new BrowserWindow when `app` is ready
 function createWindow() {
     mainWindow = new BrowserWindow({
-        width: 1000, height: 800,
-        webPreferences: {nodeIntegration: true}
-    })
+        width: 1000,
+        height: 800,
+        webPreferences: {nodeIntegration: true},
+    });
 
     // Load index.html into the new BrowserWindow
-    mainWindow.loadFile('src/index.html')
+    mainWindow.loadFile('src/index.html');
 
     // Open DevTools - Remove for PRODUCTION!
-    mainWindow.webContents.openDevTools()
+    mainWindow.webContents.openDevTools();
 
     // Listen for window being closed
     mainWindow.on('closed', () => {
-        mainWindow = null
-    })
+        mainWindow = null;
+    });
 }
 
 // Electron `app` is ready
-app.on('ready', createWindow)
+app.on('ready', createWindow);
 
 // Quit when all windows are closed - (Not macOS - Darwin)
 app.on('window-all-closed', () => {
-    if (process.platform !== 'darwin') app.quit()
-})
+    if (process.platform !== 'darwin') app.quit();
+});
 
 // When app icon is clicked and app is running, (macOS) recreate the BrowserWindow
 app.on('activate', () => {
-    if (mainWindow === null) createWindow()
-})
+    if (mainWindow === null) createWindow();
+});
