ea1042b2206237e29d37b8b1efbbf138746a4e08
zdenekd <ditezdenek@gmail.com>
2020-03-25 11:20:00 +0100

    Add electron window state package (keep window state), Play with secondary window

--- src/index.js
+++ src/index.js
@@ -4,24 +4,40 @@ const {app, BrowserWindow} = require('electron');
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
 let mainWindow;
+let secondaryWindow;
 
 // Create a new BrowserWindow when `app` is ready
 function createWindow() {
     mainWindow = new BrowserWindow({
         width: 1000,
         height: 800,
+        backgroundColor: '#2c9',
         webPreferences: {nodeIntegration: true},
+    });
+
+    secondaryWindow = new BrowserWindow({
+        width: 300,
+        height: 300,
+        parent: mainWindow,
+        show: false,
     });
 
     // Load index.html into the new BrowserWindow
     mainWindow.loadFile('src/index.html');
+    secondaryWindow.loadFile('src/index.html');
 
     // Open DevTools - Remove for PRODUCTION!
-    mainWindow.webContents.openDevTools();
+    // mainWindow.webContents.openDevTools();
+
+    // secondaryWindow.once('ready-to-show', secondaryWindow.show);
 
     // Listen for window being closed
     mainWindow.on('closed', () => {
         mainWindow = null;
+    });
+
+    secondaryWindow.on('closed', () => {
+        secondaryWindow = null;
     });
 }
 
